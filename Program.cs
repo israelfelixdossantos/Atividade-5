7414c4979b48401be37d98e88845b39057179b11
Israel Felix <grrifs@gmail.com>
2025-10-05 20:58:10 -0300

    Primeiro commit da atividade 5

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using Exo.WebApi.Contexts;
+using Exo.WebApi.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ExoContext, ExoContext>();
+builder.Services.AddControllers();
+builder.Services.AddTransient<ProjetoRepository, ProjetoRepository>();
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.MapControllers();
+
+app.Run();
